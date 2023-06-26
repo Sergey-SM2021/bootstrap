d7a9540fa169e6a570aaa49a1bd2690a99acd26a
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-06-26 15:11:31 +0300

    1 Начало разработки Основы Webpack. Добавляем TypeScript

---
+++ build/main.4fb462755e81cf6fa469.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("botstarts"),console.log("botstarts")})();
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { test } from "./test";
+
+const index = () => {
+  console.log("botstarts");
+  test();
+};
+
+index()
