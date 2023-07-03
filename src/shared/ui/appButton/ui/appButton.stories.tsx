5b6383f4f9282cd180737630bbd3010896e5db72
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-07-03 21:56:07 +0300

    setup loki

--- src/shared/ui/appButton/ui/appButton.stories.tsx
+++ src/shared/ui/appButton/ui/appButton.stories.tsx
@@ -1,38 +1,38 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react"
 
-import { AppButton, AppButtonTheme } from "./appButton";
-import { ThemeDecorator } from "shared/config/storybook/decorators/themeDecorator";
-import { Theme } from "app/providers/ThemeProvider/lib/ThemeContext";
+import { AppButton, AppButtonTheme } from "./appButton"
+import { ThemeDecorator } from "shared/config/storybook/decorators/themeDecorator"
+import { Theme } from "app/providers/ThemeProvider/lib/ThemeContext"
 
 const meta: Meta<typeof AppButton> = {
-  title: "Shared/AppButton",
-  component: AppButton,
-  tags: ["autodocs"],
-};
+	title: "Shared/AppButton",
+	component: AppButton,
+	tags: ["autodocs"],
+}
 
-export default meta;
+export default meta
 type Story = StoryObj<typeof AppButton>;
 
 export const Dark: Story = {
-  args: {
-    theme: AppButtonTheme.primary,
-    children: "Кнопка",
-  },
-  decorators: [ThemeDecorator(Theme.darkTheme)]
-};
+	args: {
+		theme: AppButtonTheme.primary,
+		children: "Кнопка",
+	},
+	decorators: [ThemeDecorator(Theme.darkTheme)],
+}
 
 export const Light: Story = {
-  args: {
-    theme: AppButtonTheme.primary,
-    children: "Кнопка",
-  },
-  decorators: [ThemeDecorator(Theme.lightTheme)]
-};
+	args: {
+		theme: AppButtonTheme.primary,
+		children: "Кнопка",
+	},
+	decorators: [ThemeDecorator(Theme.lightTheme)],
+}
 
 export const Clear: Story = {
-  args: {
-    theme: AppButtonTheme.clear,
-    children: "Кнопка",
-  },
-  decorators: [ThemeDecorator(Theme.lightTheme)]
-};
+	args: {
+		theme: AppButtonTheme.clear,
+		children: "Кнопка",
+	},
+	decorators: [ThemeDecorator(Theme.lightTheme)],
+}
