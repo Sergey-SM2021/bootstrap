387456a59d005b357ff4af8dbef70768cede9cb6
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-07-16 23:05:51 +0300

    setup json server

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line
+const jsonServer = require("json-server")
+// eslint-disable-next-line
+const fs = require("fs")
+// eslint-disable-next-line
+const path = require("path")
+const server = jsonServer.create()
+const router = jsonServer.router("db.json")
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+
+server.use((req, res, next)=> new Promise((resolve, reject) => {setTimeout(()=>{
+	resolve(res)
+	next()
+},1500)}))
+
+server.use((req, res, next) => {
+	if (!req.headers.authorization) res.status(403).json("Invalid authorization")
+	next()
+})
+
+server.post("/login", (req, res) => {
+	const {login, password} = req.body
+	// eslint-disable-next-line
+	const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, "db.json")))
+	const user = db.users.find(el => el.login === login && el.password === password)
+	if (!user) res.status(403).json("Invalid password")
+	res.send(user)
+})
+
+server.use(router)
+server.listen(3000)
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,23 @@
-import path from "path";
-import { BuildWebpackConfig } from "./config/build/buildWebpackConfig";
-import { BuildEnv, BuildPaths } from "./config/build/types/config";
+import path from "path"
+import { BuildWebpackConfig } from "./config/build/buildWebpackConfig"
+import { BuildEnv, BuildPaths } from "./config/build/types/config"
 
 export default ({ MODE = "development", PORT = 3300 }: BuildEnv) => {
   
-  const paths: BuildPaths = {
-    entry: path.resolve(__dirname, "src", "index.tsx"),
-    html: path.resolve(__dirname, "public", "index.html"),
-    output: path.resolve(__dirname, "build"),
-    src: path.resolve(__dirname, "src"),
-  };
+	const paths: BuildPaths = {
+		entry: path.resolve(__dirname, "src", "index.tsx"),
+		html: path.resolve(__dirname, "public", "index.html"),
+		output: path.resolve(__dirname, "build"),
+		src: path.resolve(__dirname, "src"),
+	}
 
-  const isDev = MODE === "development";
+	const isDev = MODE === "development"
 
 
-  return BuildWebpackConfig({
-    paths,
-    mode: MODE,
-    isDev,
-    port: PORT
-  });
-};
+	return BuildWebpackConfig({
+		paths,
+		mode: MODE,
+		isDev,
+		port: PORT
+	})
+}
