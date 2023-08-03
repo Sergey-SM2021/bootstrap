c6f6c63ddda41866fc7a34364df099c97dddc9c8
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-08-03 22:16:59 +0300

    add new theme

--- src/widgets/themeSwitcher/ui/themeSwitcher.tsx
+++ src/widgets/themeSwitcher/ui/themeSwitcher.tsx
@@ -1,16 +1,36 @@
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme"
 import Light from "../assets/light.svg"
 import Dark from "../assets/dark.svg"
+import Custom from "../assets/custom.svg"
 import { AppButton, AppButtonTheme } from "shared/ui/appButton"
 import { Theme } from "app/providers/ThemeProvider/lib/ThemeContext"
 import { memo } from "react"
 import clx from "./themeSwitcher.module.scss"
 
+interface ThemeIcon {
+  theme: Theme;
+}
+
+const ThemeIcon = ({ theme }: ThemeIcon) => {
+	switch (theme) {
+	case Theme.CustomTheme:
+		return <Custom />
+	case Theme.darkTheme:
+		return <Dark />
+	default:
+		return <Light />
+	}
+}
+
 export const ThemeSwitcher = memo(() => {
 	const { theme, toggleThemeHamdler } = useTheme()
 	return (
-		<AppButton className={clx.themeSwitcher} theme={AppButtonTheme.clear} onClick={toggleThemeHamdler}>
-			{theme === Theme.lightTheme ? <Dark /> : <Light />}
+		<AppButton
+			className={clx.themeSwitcher}
+			theme={AppButtonTheme.clear}
+			onClick={toggleThemeHamdler}
+		>
+			<ThemeIcon theme={theme}/>
 		</AppButton>
 	)
 })
