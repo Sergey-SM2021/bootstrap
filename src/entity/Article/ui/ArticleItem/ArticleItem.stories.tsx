b94a34001702fa6559cd9774ce8e18201d213187
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-08-23 17:14:25 +0300

    big article

--- src/entity/Article/ui/ArticleItem/ArticleItem.stories.tsx
+++ src/entity/Article/ui/ArticleItem/ArticleItem.stories.tsx
@@ -3,8 +3,8 @@ import { ArticleItem } from "./ArticleItem"
 import { RouterDecorator } from "shared/config/storybook/decorators/routerDecorator"
 import { ThemeDecorator } from "shared/config/storybook/decorators/themeDecorator"
 import { Theme } from "app/providers/ThemeProvider/lib/ThemeContext"
-import { ArticleLabel } from "entity/Article/model/types/Article"
-import img from "shared/assets/avatar.jpg"
+import { ArticleLabel, ArticleType } from "entity/Article/model/types/Article"
+import img from "shared/assets/assasyn.jpg"
 
 const meta: Meta<typeof ArticleItem> = {
 	title: "Entity/ArticleItem",
@@ -15,21 +15,50 @@ const meta: Meta<typeof ArticleItem> = {
 export default meta
 type Story = StoryObj<typeof ArticleItem>;
 
-export const Big: Story = {
-	args: {
-		mode: "big",
-	},
-	decorators: [RouterDecorator(), ThemeDecorator(Theme.darkTheme)],
-}
-
 export const Small: Story = {
 	args: {
-		label: [ArticleLabel.IT, ArticleLabel.ECONOMICS, ArticleLabel.MEDICINE, ArticleLabel.ECONOMICS],
+		label: [
+			ArticleLabel.IT,
+			ArticleLabel.ECONOMICS,
+			ArticleLabel.MEDICINE,
+			ArticleLabel.ECONOMICS,
+		],
 		mode: "small",
 		createdAt: "01.29.3210",
 		img,
 		title: "Assasyn creed valhalla",
-		views: 210
+		views: 210,
+	},
+	decorators: [RouterDecorator(), ThemeDecorator(Theme.darkTheme)],
+}
+
+export const Big: Story = {
+	args: {
+		label: [
+			ArticleLabel.IT,
+			ArticleLabel.ECONOMICS,
+			ArticleLabel.MEDICINE,
+			ArticleLabel.ECONOMICS,
+		],
+		mode: "big",
+		createdAt: "01.29.3210",
+		img,
+		title: "Assasyn creed valhalla",
+		views: 210,
+		blocks: [
+			{
+				id: 1,
+				type: ArticleType.TEXT,
+				title: "Заголовок этого блока",
+				paragraphs: [
+					"Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.",
+					"JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.",
+					"Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:",
+				],
+			},
+		],
+		username: "Sergeyuvbnb",
+		avatar: img
 	},
 	decorators: [RouterDecorator(), ThemeDecorator(Theme.darkTheme)],
 }
--- src/entity/Article/ui/ArticleItem/ArticleItem.tsx
+++ src/entity/Article/ui/ArticleItem/ArticleItem.tsx
@@ -1,4 +1,8 @@
-import { Article } from "entity/Article/model/types/Article"
+import {
+	Article,
+	ArticleTextBlock,
+	ArticleType,
+} from "entity/Article/model/types/Article"
 import { memo } from "react"
 import clx from "./ArticleItem.module.scss"
 import { classNames } from "shared/lib/helpers/classNames/classNames"
@@ -9,29 +13,82 @@ import Views from "shared/assets/views.svg"
 import { Icon } from "shared/ui/icon/Icon"
 import { useTranslation } from "react-i18next"
 import { useHover } from "shared/lib/hooks/useHover"
+import { AppButton, AppButtonTheme } from "shared/ui/appButton"
+import { Avatar } from "shared/ui/avatar/avatar"
 
 interface ArticleItemProps extends Article {
   mode: "big" | "small";
+  avatar: string;
+  username: string;
 }
 
 export const ArticleItem = memo((props: ArticleItemProps) => {
-	const { mode, createdAt, title, img, label, views } = props
+	const {
+		mode,
+		createdAt,
+		title,
+		img,
+		label,
+		views,
+		blocks,
+		avatar,
+		username,
+	} = props
 	const nav = useNavigate()
-	const { isHover, ...rest } = useHover()
+	const { isHover, ...hover } = useHover()
 	const { t } = useTranslation()
 
 	const handlerNavigate = () => {
 		nav("/")
 	}
 
+	const ViewsCount = (
+		<Flex align="center" gap={8}>
+			{views}
+			<Icon size="xs">
+				<Views />
+			</Icon>
+		</Flex>
+	)
+
 	if (mode === "big") {
-		return <div className={classNames(clx.big, {}, [clx.card])}></div>
+		const text = blocks.find(
+			(el) => el.type === ArticleType.TEXT
+		) as ArticleTextBlock
+
+		return (
+			<div className={classNames(clx.big, {}, [clx.card])}>
+				<Flex direction="column" gap={8} className={clx.header}>
+					<Flex justify="space-between" align="center">
+						<Flex align="center" gap={16}>
+							<Avatar size="xs" src={avatar} />
+							<Text>{username}</Text>
+						</Flex>
+						<div>{createdAt}</div>
+					</Flex>
+					<Text size={TextSize.lg}>{title}</Text>
+					<p className={clx.labels}>{label.join(", ")}</p>
+				</Flex>
+				<div className={clx.img} style={{ backgroundImage: `URL(${img})` }} />
+				{text.paragraphs.map((el, index) => (
+					<p key={index} className={clx.paragraph}>
+						{el}
+					</p>
+				))}
+				<Flex justify="space-between" align="center" className={clx.footer}>
+					<AppButton onClick={handlerNavigate} theme={AppButtonTheme.primary}>
+						{t("read more")}
+					</AppButton>
+					{ViewsCount}
+				</Flex>
+			</div>
+		)
 	}
 
 	return (
 		<div
 			className={classNames(clx.small, { [clx.hovered]: isHover }, [clx.card])}
-			{...rest}
+			{...hover}
 		>
 			<div className={clx.content} style={{ backgroundImage: `url(${img})` }}>
 				<div className={clx.createdAt}>{createdAt}</div>
@@ -39,12 +96,7 @@ export const ArticleItem = memo((props: ArticleItemProps) => {
 			<div className={clx.footer}>
 				<Flex gap={16} align="center">
 					<p className={clx.labels}>{label.join(", ")}</p>
-					<Flex align="center" gap={8}>
-						{views}
-						<Icon size="xs">
-							<Views />
-						</Icon>
-					</Flex>
+					{ViewsCount}
 				</Flex>
 				<Text size={TextSize.lg}>{title}</Text>
 				<div className={clx.readTheText} onClick={handlerNavigate}>
