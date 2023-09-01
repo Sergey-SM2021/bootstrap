addff79dad4aaf4528dc1995a80cdb360d5461aa
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-09-01 15:06:16 +0300

    fix: not found page

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,7 +1,12 @@
 import { useTranslation } from "react-i18next"
 import style from "./NotFoundPage.module.scss"
+import { Layout } from "shared/ui/Layout/Layout"
 
 export const NotFoundPage = () => {
 	const { t } = useTranslation()
-	return <div className={style.NotFoundPage}>{t("not found page")}</div>
+	return (
+		<Layout>
+			<div className={style.NotFoundPage}>{t("not found page")}</div>
+		</Layout>
+	)
 }
