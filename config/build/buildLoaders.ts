23816a53304a4c6bfe16fabf04dd9560af3f6610
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-09-01 20:53:37 +0300

    fix: eslint

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,49 +1,49 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import { BuildOptions } from "./types/config";
+import webpack from "webpack"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import { BuildOptions } from "./types/config"
 
 export function BuildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: "ts-loader",
-    exclude: /node_modules/,
-  };
+	const typescriptLoader = {
+		test: /\.tsx?$/,
+		use: "ts-loader",
+		exclude: /node_modules/,
+	}
 
-  const svgLoader = {
-    test: /\.svg$/i,
-    issuer: /\.tsx?$/,
-    use: ["@svgr/webpack"],
-  };
+	const svgLoader = {
+		test: /\.svg$/i,
+		issuer: /\.tsx?$/,
+		use: ["@svgr/webpack"],
+	}
 
-  const babel = {
-    test: /\.(ts|js|tsx|jsx)/,
-    exclude: /node_modules/,
-    use: {
-      loader: "babel-loader",
-    },
-  };
+	const babel = {
+		test: /\.(ts|js|tsx|jsx)/,
+		exclude: /node_modules/,
+		use: {
+			loader: "babel-loader",
+		},
+	}
 
-  const scssLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      // Translates CSS into CommonJS
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            auto: (resourcePath: string) =>
-              resourcePath.endsWith(".module.scss"),
-            localIdentName: isDev
-              ? "[path][name]__[local]--[hash:base64:5]"
-              : "[hash:base64:8]",
-          },
-        },
-      },
-      // Compiles Sass to CSS
-      "sass-loader",
-    ],
-  };
+	const scssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+			// Translates CSS into CommonJS
+			{
+				loader: "css-loader",
+				options: {
+					modules: {
+						auto: (resourcePath: string) =>
+							resourcePath.endsWith(".module.scss"),
+						localIdentName: isDev
+							? "[path][name]__[local]--[hash:base64:5]"
+							: "[hash:base64:8]",
+					},
+				},
+			},
+			// Compiles Sass to CSS
+			"sass-loader",
+		],
+	}
 
-  return [babel, typescriptLoader, svgLoader, scssLoader];
+	return [babel, typescriptLoader, svgLoader, scssLoader]
 }
--- config/build/buildResolve.ts
+++ config/build/buildResolve.ts
@@ -1,10 +1,10 @@
-import webpack from "webpack";
-import { BuildOptions } from "./types/config";
+import webpack from "webpack"
+import { BuildOptions } from "./types/config"
 
 export function BuildResolve({ paths }: BuildOptions): webpack.ResolveOptions {
-  return {
-    extensions: [".tsx", ".ts", ".js"],
-    modules: [paths.src, "node_modules"],
-    preferAbsolute: true,
-  };
+	return {
+		extensions: [".tsx", ".ts", ".js"],
+		modules: [paths.src, "node_modules"],
+		preferAbsolute: true,
+	}
 }
