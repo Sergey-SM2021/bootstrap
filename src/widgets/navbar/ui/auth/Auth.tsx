899b4629e00d0b309dd37f53450f7d8f330c7efd
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-09-07 10:55:54 +0300

    fix: paddings

--- src/widgets/navbar/ui/auth/Auth.tsx
+++ src/widgets/navbar/ui/auth/Auth.tsx
@@ -7,22 +7,24 @@ import Profile from "shared/assets/profile.svg"
 import { RouterPaths } from "shared/config/routerConfig/RouterConfig"
 import { useNavigate } from "react-router-dom"
 import { logout } from "entity/user/model/slice/userSlice"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useTranslation } from "react-i18next"
 import clx from "../../style/navbar.module.scss"
+import { getUser } from "entity/user/model/selector/getUserSelector"
 
 export const Auth = memo(() => {
 	const nav = useNavigate()
 	const dispatch = useDispatch()
 	const { t } = useTranslation()
+	const currrentUserId = useSelector(getUser)?.id
 
 	const handlerCreatePost = useCallback(() => {
 		nav(RouterPaths.article_create)
 	}, [nav])
 
 	const handlerNavgate = useCallback(() => {
-		nav(RouterPaths.profile)
-	}, [nav])
+		nav(`${RouterPaths.profile}${currrentUserId}`)
+	}, [nav, currrentUserId])
 
 	const logoutHandler = useCallback(async () => {
 		dispatch(logout())
