258f7a261938f3a05c2cdb5f6fff9972436204d8
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-09-13 22:15:12 +0300

    update cypress test

--- cypress/e2e/passed.cy.ts
+++ cypress/e2e/passed.cy.ts
@@ -1,37 +1,20 @@
 describe("Auth flow", () => {
-	
-	it.only("logined user can visit private page", () => {
-		cy.visit("http://localhost:8081/")
-		
-		cy.url().should("equal", "http://localhost:8081/")
+	beforeEach(() => {
+		cy.login()
+	})
 
-		cy.visit("http://localhost:8081/articles")
-
-		cy.contains(/войти/i).click()
-
-		cy.get("input").eq(0).type("sergey2003.k.96@gmail.com")
-
-		cy.get("input").eq(1).type("sergey2003")
-
-		cy.get("button").contains(/войти/i).click()
+	it("logined user can visit private page", () => {
+		cy.visit(`${Cypress.config().baseUrl}articles`)
 
 		cy.contains(/войти/i).should("not.exist")
 
-		cy.visit("http://localhost:8081/articles")
+		cy.visit(`${Cypress.config().baseUrl}articles`)
 
-		cy.url().should("equal", "http://localhost:8081/articles")
+		cy.url().should("equal", `${Cypress.config().baseUrl}articles`)
 	})
 
 	it("logout work correctly", () => {
-		cy.visit("http://localhost:8081/")
-
-		cy.contains(/войти/i).click()
-
-		cy.get("input").eq(0).type("sergey2003.k.96@gmail.com")
-
-		cy.get("input").eq(1).type("sergey2003")
-
-		cy.get("button").contains(/войти/i).click()
+		cy.visit("/")
 
 		cy.contains(/войти/i).should("not.exist")
 
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,26 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+import { USER_LOCALSTORAGE_NAME } from "../../src/shared/const/localstorage"
+
+Cypress.Commands.add(
+	"login",
+	(login = "sergey2003.k.96@gmail.com", password = "sergey2003") => {
+		cy.request({
+			method: "POST",
+			body: {
+				login,
+				password,
+			},
+			url: "http://localhost:4200/api/auth/login",
+		}).then(({body}) => localStorage.setItem(USER_LOCALSTORAGE_NAME, JSON.stringify(body)))
+	}
+)
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      login(email?: string, password?: string): Chainable<void>;
+    }
+  }
+}
+
+export {}
