0eae74a5325016a92e607f2028792c6c44a757de
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-09-17 21:58:57 +0300

    add prettier

--- src/entity/Article/ui/ArticleItem/ArticleItem.tsx
+++ src/entity/Article/ui/ArticleItem/ArticleItem.tsx
@@ -1,103 +1,107 @@
-import { memo, useMemo } from "react"
-import clx from "./ArticleItem.module.scss"
+import { AppButton, AppButtonTheme } from "shared/ui/appButton"
+
+import Views from "shared/assets/views.svg"
 import { classNames } from "shared/lib/helpers/classNames/classNames"
-import { useNavigate } from "react-router-dom"
+import { useHover } from "shared/lib/hooks/useHover"
 import { Flex } from "shared/ui/Flex/Flex"
 import { Text, TextSize } from "shared/ui/Text/Text"
-import Views from "shared/assets/views.svg"
-import { Icon } from "shared/ui/icon/Icon"
-import { useTranslation } from "react-i18next"
-import { useHover } from "shared/lib/hooks/useHover"
-import { AppButton, AppButtonTheme } from "shared/ui/appButton"
 import { Avatar } from "shared/ui/avatar/avatar"
+import { Icon } from "shared/ui/icon/Icon"
+
 import {
-	ArticleTextBlock,
-	ArticleType,
-	ArticleMode,
-	ArticleParagraph,
+  ArticleMode,
+  ArticleParagraph,
+  ArticleTextBlock,
+  ArticleType,
 } from "entity/ArticleDetalis"
 
+import clx from "./ArticleItem.module.scss"
+
+import { memo, useMemo } from "react"
+import { useTranslation } from "react-i18next"
+import { useNavigate } from "react-router-dom"
+
 interface ArticleItemProps extends ArticleType {
-  mode: "big" | "small";
-  className?: string;
+  mode: "big" | "small"
+  className?: string
 }
 
 export const ArticleItem = memo((props: ArticleItemProps) => {
-	const { mode, createdAt, title, img, label, views, blocks, user, id } = props
-	const nav = useNavigate()
-	const { isHover, ...hover } = useHover()
-	const { t } = useTranslation()
+  const { mode, createdAt, title, img, label, views, blocks, user, id } = props
+  const nav = useNavigate()
+  const { isHover, ...hover } = useHover()
+  const { t } = useTranslation()
 
-	const handlerNavigate = () => {
-		nav(`/article/${id}`)
-	}
+  const handlerNavigate = () => {
+    nav(`/article/${id}`)
+  }
 
-	const ViewsCount = useMemo(
-		() => (
-			<Flex align="center" gap={8}>
-				{views}
-				<Icon size="xs">
-					<Views />
-				</Icon>
-			</Flex>
-		),
-		[views]
-	)
+  const ViewsCount = useMemo(
+    () => (
+      <Flex align="center" gap={8}>
+        {views}
+        <Icon size="xs">
+          <Views />
+        </Icon>
+      </Flex>
+    ),
+    [views],
+  )
 
-	const handlerNavigateOnUser = () => {
-		nav(`/profile/${user.id}`)
-	}
+  const handlerNavigateOnUser = () => {
+    nav(`/profile/${user.id}`)
+  }
 
-	if (mode === "big") {
-		const text = blocks.find(
-			(el) => el.type === ArticleMode.TEXT
-		) as ArticleTextBlock
+  if (mode === "big") {
+    const text = blocks.find(
+      (el) => el.type === ArticleMode.TEXT,
+    ) as ArticleTextBlock
 
-		return (
-			<div className={classNames(clx.big, {}, [clx.card])}>
-				<Flex direction="column" gap={8} className={clx.header}>
-					<Flex justify="space-between" align="center">
-						<Flex
-							align="center"
-							gap={16}
-							className={clx.UserLink}
-							onClick={handlerNavigateOnUser}
-						>
-							<Avatar size="xs" src={user.avatar} />
-							<Text>{user.nickname}</Text>
-						</Flex>
-						<div>{createdAt}</div>
-					</Flex>
-					<Text size={TextSize.lg}>{title}</Text>
-					<p className={clx.labels}>{label.join(", ")}</p>
-				</Flex>
-				<div className={clx.img} style={{ backgroundImage: `URL(${img})` }} />
-				<ArticleParagraph block={text} className={clx.content} />
-				<Flex justify="space-between" align="end" className={clx.footer}>
-					<AppButton onClick={handlerNavigate} theme={AppButtonTheme.primary}>
-						{t("read more")}
-					</AppButton>
-					<div>{ViewsCount}</div>
-				</Flex>
-			</div>
-		)
-	}
+    return (
+      <div className={classNames(clx.big, {}, [clx.card])}>
+        <Flex direction="column" gap={8} className={clx.header}>
+          <Flex justify="space-between" align="center">
+            <Flex
+              align="center"
+              gap={16}
+              className={clx.UserLink}
+              onClick={handlerNavigateOnUser}
+            >
+              <Avatar size="xs" src={user.avatar} />
+              <Text>{user.nickname}</Text>
+            </Flex>
+            <div>{createdAt}</div>
+          </Flex>
+          <Text size={TextSize.lg}>{title}</Text>
+          <p className={clx.labels}>{label.join(", ")}</p>
+        </Flex>
+        <div className={clx.img} style={{ backgroundImage: `URL(${img})` }} />
+        <ArticleParagraph block={text} className={clx.content} />
+        <Flex justify="space-between" align="end" className={clx.footer}>
+          <AppButton onClick={handlerNavigate} theme={AppButtonTheme.primary}>
+            {t("read more")}
+          </AppButton>
+          <div>{ViewsCount}</div>
+        </Flex>
+      </div>
+    )
+  }
 
-	return (
-		<div className={classNames(clx.small, {}, [clx.card])} {...hover}>
-			<div className={clx.content} style={{ backgroundImage: `url(${img})` }}>
-				<div className={clx.createdAt}>{createdAt}</div>
-			</div>
-			<div className={clx.footer}>
-				<Flex gap={16} align="center">
-					<p className={clx.labels}>{label.join(", ")}</p>
-					{ViewsCount}
-				</Flex>
-				<Text size={TextSize.lg}>{title}</Text>
-				<div className={clx.readTheText} onClick={handlerNavigate}>
-					{t("read more")}
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className={classNames(clx.small, {}, [clx.card])} {...hover}>
+      <div className={clx.content} style={{ backgroundImage: `url(${img})` }}>
+        <div className={clx.createdAt}>{createdAt}</div>
+      </div>
+      <div className={clx.footer}>
+        <Flex gap={16} align="center">
+          <p className={clx.labels}>{label.join(", ")}</p>
+          {ViewsCount}
+        </Flex>
+        <Text size={TextSize.lg}>{title}</Text>
+        <div className={clx.readTheText} onClick={handlerNavigate}>
+          {t("read more")}
+        </div>
+      </div>
+    </div>
+  )
 })
--- src/widgets/navbar/ui/auth/Auth.tsx
+++ src/widgets/navbar/ui/auth/Auth.tsx
@@ -1,69 +1,75 @@
-import { memo, useCallback, useMemo } from "react"
-import { Flex } from "shared/ui/Flex/Flex"
 import { AppButton, AppButtonTheme } from "shared/ui/appButton"
-import { MyDropdown } from "shared/ui/menu/Dropdown"
-import { Logo } from "widgets/Logo"
-import { useNavigate } from "react-router-dom"
-import { actions, userSelectors, Role } from "entity/user"
+
 import { Notifications } from "features/notifications"
-import { Avatar } from "shared/ui/avatar/avatar"
-import { useDispatch, useSelector } from "react-redux"
-import { useTranslation } from "react-i18next"
-import clx from "../../style/navbar.module.scss"
+
+import { Logo } from "widgets/Logo"
+
+import { Role, actions, userSelectors } from "entity/user"
+
 import { GetRouter } from "shared/const/router"
+import { Flex } from "shared/ui/Flex/Flex"
+import { Avatar } from "shared/ui/avatar/avatar"
+import { MyDropdown } from "shared/ui/menu/Dropdown"
+
+import clx from "../../style/navbar.module.scss"
+
+import { memo, useCallback, useMemo } from "react"
+import { useTranslation } from "react-i18next"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
 export const Auth = memo(() => {
-	const nav = useNavigate()
-	const dispatch = useDispatch()
-	const { t } = useTranslation()
-	const currrentUserId = useSelector(userSelectors.getUserId)
-	const userAvatar = useSelector(userSelectors.getUserAvatar)
-	const userRole = useSelector(userSelectors.getUserRole)
+  const nav = useNavigate()
+  const dispatch = useDispatch()
+  const { t } = useTranslation()
+  const currrentUserId = useSelector(userSelectors.getUserId)
+  const userAvatar = useSelector(userSelectors.getUserAvatar)
+  const userRole = useSelector(userSelectors.getUserRole)
 
-	const handlerCreatePost = useCallback(() => {
-		nav(GetRouter.NewArticle())
-	}, [nav])
+  const handlerCreatePost = useCallback(() => {
+    nav(GetRouter.NewArticle())
+  }, [nav])
 
-	const handlerNavgate = useCallback(() => {
-		nav(GetRouter.Profile(currrentUserId as string))
-	}, [nav, currrentUserId])
+  const handlerNavgate = useCallback(() => {
+    nav(GetRouter.Profile(currrentUserId as string))
+  }, [nav, currrentUserId])
 
-	const handlerAnalyticsNavgate = useCallback(() => {
-		nav("/analytics")
-	}, [nav])
+  const handlerAnalyticsNavgate = useCallback(() => {
+    nav("/analytics")
+  }, [nav])
 
-	const logoutHandler = useCallback(async () => {
-		dispatch(actions.logout())
-	}, [dispatch])
+  const logoutHandler = useCallback(async () => {
+    dispatch(actions.logout())
+  }, [dispatch])
 
-	const links = useMemo(
-		() =>
-			[
-				{ onClick: logoutHandler, text: t("logout") },
-				{ onClick: handlerNavgate, text: t("profile") },
-				userRole === Role.admin && {
-					onClick: handlerAnalyticsNavgate,
-					text: t("analytics"),
-				},
-			].filter(Boolean),
-		[logoutHandler, handlerNavgate, handlerAnalyticsNavgate, t, userRole]
-	)
+  const links = useMemo(
+    () =>
+      [
+        { onClick: logoutHandler, text: t("logout") },
+        { onClick: handlerNavgate, text: t("profile") },
+        userRole === Role.admin && {
+          onClick: handlerAnalyticsNavgate,
+          text: t("analytics"),
+        },
+      ].filter(Boolean),
+    [logoutHandler, handlerNavgate, handlerAnalyticsNavgate, t, userRole],
+  )
 
-	return (
-		<Flex align="center" gap={32} className={clx.navbar}>
-			<Logo />
-			<AppButton onClick={handlerCreatePost} theme={AppButtonTheme.primary}>
-				{t("create post")}
-			</AppButton>
-			<div className={clx.links}></div>
-			<Notifications />
-			<MyDropdown
-				Trigger={<Avatar src={userAvatar} size="xs" />}
-				// @ts-ignore
-				items={links}
-				top={"100%"}
-				right={0}
-			/>
-		</Flex>
-	)
+  return (
+    <Flex align="center" gap={32} className={clx.navbar}>
+      <Logo />
+      <AppButton onClick={handlerCreatePost} theme={AppButtonTheme.primary}>
+        {t("create post")}
+      </AppButton>
+      <div className={clx.links}></div>
+      <Notifications />
+      <MyDropdown
+        Trigger={<Avatar src={userAvatar} size="xs" />}
+        // @ts-ignore
+        items={links}
+        top={"100%"}
+        right={0}
+      />
+    </Flex>
+  )
 })
